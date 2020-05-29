194bf1281e36b0558a5e82d5a83398879206289c
maykonmichel <maykonmichelpalma@gmail.com>
2020-05-29 13:31:48 -0300

    Configured eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'airbnb', 'prettier', 'prettier/react'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': [
+      'error',
+      {
+        ignore: ['children'],
+      },
+    ],
+    'no-console': 'error',
+  },
 };
