f6647f7ce99d030e55240937897e6cd6172d0ff1
maykonmichel <maykonmichelpalma@gmail.com>
2020-05-29 15:27:06 -0300

    Added show component

---
+++ src/components/Show/styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1 / 2,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  image: {
+    width: 180,
+    height: 253,
+  },
+});
