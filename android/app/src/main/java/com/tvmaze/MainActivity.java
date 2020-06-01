62db8471af1987c4d6d39a0ae346748492e32d52
maykonmichel <maykonmichelpalma@gmail.com>
2020-06-01 14:59:39 -0300

    Added splash screen

--- android/app/src/main/java/com/tvmaze/MainActivity.java
+++ android/app/src/main/java/com/tvmaze/MainActivity.java
@@ -1,15 +1,24 @@
 package com.tvmaze;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+import org.devio.rn.splashscreen.SplashScreen;
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "tvMaze";
-  }
+public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "tvMaze";
+    }
 }
