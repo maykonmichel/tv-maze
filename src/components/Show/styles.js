ad6c745cc5185e329588f3dbac550722fe514ac3
maykonmichel <maykonmichelpalma@gmail.com>
2020-06-01 09:15:08 -0300

    Styled show component

--- src/components/Show/styles.js
+++ src/components/Show/styles.js
@@ -1,13 +1,29 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
+  average: {
+    backgroundColor: '#534bae',
+    color: '#fdd835',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
   container: {
     flex: 1 / 2,
     alignItems: 'center',
     marginVertical: 10,
   },
+  header: {
+    backgroundColor: '#000051',
+    height: 40,
+    justifyContent: 'center',
+  },
   image: {
     width: 180,
     height: 253,
+  },
+  name: {
+    color: '#fff',
+    textAlign: 'center',
+    textTransform: 'uppercase',
   },
 });
