a9f45629a0e90f304072ec4e65190d9369b20b5b
maykonmichel <maykonmichelpalma@gmail.com>
2020-06-01 20:15:10 -0300

    Added favorites query

--- src/store/resolvers/Query/index.js
+++ src/store/resolvers/Query/index.js
@@ -1,9 +1,11 @@
 import episode from './episode';
+import favorites from './favorites';
 import show from './show';
 import shows from './shows';
 
 export default {
   episode,
+  favorites,
   show,
   shows,
 };
